517618144f98a01f78aebb9d60873bc4189417e0
Jesse Burnette <tubbyburnette@gmail.com>
2022-04-28 17:51:22 -0400

    beepboop

--- practice-for-week-03-additional-practice-main/problems/01-r2d2-speaks.js
+++ practice-for-week-03-additional-practice-main/problems/01-r2d2-speaks.js
@@ -2,29 +2,46 @@
     Write a function r2d2Speaks that takes in a series of 0s and 1s.
     The function should console.log a 'beep' for a 0 followed by a pause of 400 ms and a 'boop' for a 1 followed by 800 ms.
     
-    let code = [0, 1, 1, 0];
-    r2d2Speaks(code);
-        print 'beep'
-        pause for 400ms
-        print 'boop'
-        pause for 800ms
-        print 'boop'
-        pause for 800ms
-        print 'beep'
-        pause for 400ms
-
+    print 'beep'
+    pause for 400ms
+    print 'boop'
+    pause for 800ms
+    print 'boop'
+    pause for 800ms
+    print 'beep'
+    pause for 400ms
+    
     There are no mocha tests for this code.
     Run the following two lines beneath the function
     to see if your code demonstrates the expected behavior.
+    
+    */
    
-*/
+   function r2d2Speaks(code, time = 0) {
+       debugger
+       if(code.length === 0 ){
+           return;
+        }
+        let firstEl = code.shift()
+        if(firstEl === 0){
+            setTimeout(() => {
+                console.log('beep')
+            },time += 400)
+        }
+        else { 
+            setTimeout(() =>{
+                console.log('boop')
+            },time += 800)
+        }
+        return r2d2Speaks(code, time);
+    }
+    
+    
+    
+    let code = [0, 1, 1, 0];
+    r2d2Speaks(code);
 
-function r2d2Speaks(code) {
-    // Your code here
-}
 
-let code = [0, 1, 1, 0];
-r2d2Speaks(code);
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
